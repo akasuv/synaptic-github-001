791d20debbae7fd61b1060c083877912e1e56550
Wei Su <me@weisu.io>
2025-02-17 10:20:36 +0800

    Add a router to return a random number

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const http = require('http');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Synaptic\n');
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === '/random') {
+    const randomNumber = Math.floor(Math.random() * 100);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ random: randomNumber }));
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, Synaptic\n');
+  }
 });
 
 server.listen(port, hostname, () => {
